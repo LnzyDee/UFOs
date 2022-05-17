2accf225ea0e6fcd176d65b1c1595e384cab5a09
unknown <lindsay.dove@gmail.com>
2022-05-17 10:09:49 -0500

    app update

--- static/js/app.js
+++ static/js/app.js
@@ -3,3 +3,22 @@ const tableData = data;
 
 // Reference the HTML table using d3
 var tbody = d3.select("tbody");
+
+//create new function
+function buildTable(data) {
+    // First, clear out any existing data
+    tbody.html("");
+
+    //next, loop through each object in the data
+    data.forEach((dataRow) => {
+        // append a arow to the table body
+        let row = tbody.append("tr");
+
+        // loop through each field in the datarow and add
+        // each value as a table cell (td)
+        Object.values(dataRow).forEach((val) => {
+         let cell = row.append("td");
+         cell.text(val);   
+        })
+    })
+}
